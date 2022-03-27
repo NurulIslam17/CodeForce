2cf3633c74983f14306093d611b8abcc4dec228e
Md Nurul Islam <98460077+NurulIslam17@users.noreply.github.com>
2022-03-27 09:13:19 +0600

    codeforce

--- Codeforce/A_BeautifulYear.cpp
+++ Codeforce/A_BeautifulYear.cpp
@@ -1,16 +1,23 @@
 #include<iostream>
-#include<string>
 using namespace std;
 int main()
 {
-    int y,rem=0;
+    int y,d4,d3,d2,d1;
     cin>>y;
     while(true)
     {
-        while(y!=0)
-        {
-            rem=y%10;
-        }
+        y++;
+        d4= y%10;
+        d3 = (y/10)%10;
+        d2= (y/100)%10;
+        d1= (y/1000)%10;
 
+        if(d1!=d2 && d1!=d3 && d1!=d4 && d2!=d3 && d2!=d4 && d3!=d4)
+        {
+            break;
+        }
     }
+
+    cout<<y<<endl;
+    return 0;
 }
