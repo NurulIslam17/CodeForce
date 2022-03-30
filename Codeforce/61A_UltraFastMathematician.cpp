10bd711469275650e1bd57ad25e4eeb6b5d6e35a
Md Nurul Islam <98460077+NurulIslam17@users.noreply.github.com>
2022-03-30 11:10:08 +0600

    codeforce

--- Codeforce/61A_UltraFastMathematician.cpp
+++ Codeforce/61A_UltraFastMathematician.cpp
@@ -1,1 +1,22 @@
 
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string str1,str2;
+    cin>>str1>>str2;
+    for(int i=0; i<str1.length(); i++)
+    {
+        if(str1[i]== str2[i])
+        {
+            str1[i]='0';
+        }else
+        {
+            str1[i]='1';
+        }
+    }
+    cout<<str1<<endl;
+    return 0;
+
+}
