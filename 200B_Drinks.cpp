9397240dc219f691dec24d62f376435500994a3c
Md Nurul Islam <98460077+NurulIslam17@users.noreply.github.com>
2022-03-30 14:03:40 +0600

    codeforce

---
+++ 200B_Drinks.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,p;
+    float  sum=0;
+    cin>>n;
+    for(int i=0;i<n; i++)
+    {
+        cin>>p;
+        sum+=p;
+    }
+    cout<<sum/n<<endl;
+    return 0;
+}
