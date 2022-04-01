9d223e853c7d7624c7e4fc9c60790512b4e18fa1
Md Nurul Islam <98460077+NurulIslam17@users.noreply.github.com>
2022-04-01 22:30:44 +0600

    Code force

---
+++ Codeforce/AAntonandLetters.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    int count=0;
+    string s;
+    getline(cin,s);
+    sort(s.begin(),s.end());
+
+    for(int i=0; i<s.size()-1; i++)
+    {
+        if(s[i]=='{' ||s[i]=='}'  || s[i]==',' || s[i]==' ')
+        {
+            continue;
+        }
+        else
+        {
+            if(s[i]!=s[i+1])
+            {
+                count++;
+            }
+        }
+    }
+    cout<<count<<endl;
+}
